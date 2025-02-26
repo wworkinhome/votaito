dabfa790c31b977ea861d65aa0cf062e33ce10bd
Wilmer Antonio Mosquera Figueroa <114489499+wworkinhome@users.noreply.github.com>
2025-02-26 08:24:13 -0500

    Update conn.php
    Actualización conexión de administrador

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'vota');
+	$conn = new mysqli('localhost', 'root', 'Sofi@3110@@**', 'vota');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
